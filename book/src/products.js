e15d72818d473f440a4be94dd0229661b06870f9
gamerraming <gamerraming@gmail.com>
2024-01-13 12:03:56 +0700

    code_react_tmp

---
+++ book/src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/products/1.png';
+import product2 from './assets/products/2.png';
+import product3 from './assets/products/3.png';
+import product4 from './assets/products/4.png';
+import product5 from './assets/products/5.png';
+import product6 from './assets/products/6.webp';
+import product7 from './assets/products/7.webp';
+import product8 from './assets/products/8.webp';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Laptop",
+        price: 123.5,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Camera",
+        price: 333.3,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Jacket",
+        price: 120.3,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Led",
+        price: 1023.7,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "T-shirt 1",
+        price: 100.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "T-shirt 2",
+        price: 120.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Hat",
+        price: 931.9,
+        productImage: product8,
+    },
+];
