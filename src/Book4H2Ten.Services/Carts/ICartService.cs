c268214f0bcf9b2521c6f3aa507a77f2db679ae7
KimTienSinh <thikimhoa.py@gmail.com>
2024-02-10 17:52:22 +0700

    add CRUD Cart

---
+++ src/Book4H2Ten.Services/Carts/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using Book4H2Ten.Services.Carts.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book4H2Ten.Services.Carts
+{
+    public interface ICartService
+    {
+        Task<CartDtos> GetCartAsync(Guid cartId);
+        Task<CartDtos> CreateCartAsync(CartDtos cartDtos, Guid bookId, Guid userId);
+        Task<CartDtos> EditCartAsync(CartDtos cartDtos, Guid cartId);
+        Task DeleteCartAsync(Guid cartId);
+
+    }
+}
